cfa9c5ef8031f2f3aae246e92ec12d4104421274
Huzaif <huzaif7999@gmail.com>
2024-07-29 15:48:56 +0530

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Huzaif</h1>
+        <p className={styles.description}>
+        Crafting Dynamic Front-End Interfaces, Robust Back-End Solutions, and Mastering Data Structures for Optimal Performance
+        </p>
+        <a href="mailto:huzaif7999@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
